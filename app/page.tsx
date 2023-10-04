5e5d8b01bc146870191a712fbdbf7562a3c5c211
Azriel <azrihell.l@gmail.com>
2023-10-04 11:03:06 -0500

    added arrow image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image"
+
 const Home = () => {
   return (
     <>
       <section className="px-6 md:px-20 py-24border-2 border-red-500">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
-            <p></p>
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image
+                src='/assets/icons/arrow-right.svg'
+                alt='arrow-right'
+                width={16}
+                height={16}
+              />
+            </p>
+            
           </div>
         </div>
       </section>
