9b73b94819d2f23cbf1ce0cd2e6728b78758d1a8
Azriel <azrihell.l@gmail.com>
2023-11-14 15:40:28 -0600

    gave type 'any' to error

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -2,5 +2,11 @@
 
 export async function scrapeAndStoreProduct(productUrl: string) {
 
-  
+  if (!productUrl) return
+
+  try {
+
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`)
+  }
 }
