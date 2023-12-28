b2242e9ebb8b32c5acf8ec6dbfcc25f7a29e9129
Azriel <azrihell.l@gmail.com>
2023-12-28 15:04:11 -0600

    started brightdata proxy configuration

--- lib/actions/scraper/index.ts
+++ lib/actions/scraper/index.ts
@@ -1,4 +1,7 @@
 export async function scrapeAmazonProduct(url: string) {
-  if(!url) return
-  
+  if (!url) return
+
+  // BrightData proxy config
+  const username = String(process.env.BRIGHT_DATA_USERNAME)
+  const password = String(process.env.BRIGHT_DATA_PASSWORD)
 }
